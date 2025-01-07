fc4a129669b0e83fa6d1f89135e8a2c7c38a44e1
Azlaan Khan Pathan <akpglb@gmail.com>
2025-01-07 10:39:36 +0530

    test-application

---
+++ news-web-app/src/app/page.tsx
@@ -0,0 +1,7 @@
+// Next js uses typescript to export components 
+
+export default function hello() {
+  return(                
+    <h1 className="text-3xl text-center mt-8">Hello World</h1>
+  )
+}
