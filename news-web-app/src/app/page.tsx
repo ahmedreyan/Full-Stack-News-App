dd32dacd741e1d42d6edfb3f7f6be82bdefa6bb3
Asif ahmed <92877147+ahmedreyan@users.noreply.github.com>
2025-01-07 11:21:58 +0530

    Delete news-web-app directory

--- news-web-app/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-// Next js uses typescript to export components 
-
-export default function hello() {
-  return(                
-    <h1 className="text-3xl text-center mt-8">Hello World</h1>
-  )
-}
